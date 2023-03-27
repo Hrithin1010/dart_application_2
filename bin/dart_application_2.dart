f01b57af00c37ae1e3e9507fd21071b51c92990e
Hrithin1010 <hrithinak@gmail.com>
2023-03-27 11:07:03 +0530

    commit

---
+++ bin/dart_application_2.dart
@@ -0,0 +1,18 @@
+void main(){
+  /// variable declaration
+  /// datatype variable_name = value;
+   String name = "Hrithin";
+   int age = 22;
+   int phone = 7356875908;
+   double mark = 6.6;
+   String email = "hrithinak@gmail.com";
+
+
+   print("student details");
+   print("my name is $name");
+  print("i am $age yrs old");
+   print("my phone number is $phone");
+   print("my mark is $mark");
+   print("my email is $email");
+
+}
