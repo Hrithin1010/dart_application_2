9503e94667ef035f17abedf28a036d0ed00ca229
Hrithin1010 <hrithinak@gmail.com>
2023-03-29 10:48:05 +0530

    commit

--- dart_application_1/bin/classdemo.dart
+++ dart_application_1/bin/classdemo.dart
@@ -8,6 +8,7 @@ class Students{
   double? cgpa;
   String? email;
 
+
   /// static variable
       static String course = "FLUTTER";
 
@@ -27,14 +28,16 @@ void main(){
   print("Phone   :${st1.phone = 7356875908}");
   print("CGPA    :${st1.cgpa  = 50}");
   print("Email   :${st1.email ="joseph@gmail.com"}");
+  print("course   : ${Students.course}");
   
   
    print("Student 2 details");
-  print("Name    :${st1.name = "hrithin"} ");
-  print("Age     :${st1.age =  22}  ");
-  print("Phone   :${st1.phone = 7356875908}");
-  print("CGPA    :${st1.cgpa  = 50}");
-  print("Email   :${st1.email ="hrithin1010@gmail.com"}");
+  print("Name    :${st2.name = "hrithin"} ");
+  print("Age     :${st2.age =  22}  ");
+  print("Phone   :${st2.phone = 7356875908}");
+  print("CGPA    :${st2.cgpa  = 50}");
+  print("Email   :${st2.email ="hrithin1010@gmail.com"}");
+  print("course   : ${Students.course}");
 
   
 }
