73219b924ca07fe191f0c708b071faa41454b6c8
Hrithin1010 <hrithinak@gmail.com>
2023-04-03 11:22:04 +0530

    commit 6

---
+++ dart_application_1/bin/iterationstatements.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+
+void main(){
+
+  //for loop
+//   for(int count=1;count<=10;count++){
+//   print(" $count");
+// }
+// }
+
+
+//while loop
+
+// int i=1;
+// while(i<=10){
+//   print(i);
+//   i++;
+// }
+// }
+
+
+//do while
+
+// int j=0;
+// do{
+//   print(j);
+//   j++;
+// }while(j<=10);
+// }
+
+
+// int sum=0;
+// for(int num=1;num<=10;num++){
+//   sum=sum+num;
+// }
+// print('sum=$sum');
+// }
+
+// even numbers sum
+// for(int i=1;i<=20;i++){
+//   if(i % 2 == 0){                   //for odd number use   !=
+//     print(i);
+//   }
+// }
+
+// }
+
+//skipping even numbers
+
+// for(int i=1;i<=20;i++){
+//   if(i % 2 == 0){
+//     continue;
+//   }
+//    print(i);
+// }
+
+
+
+//sum of even and odd numbers 1-10
+
+
+// int sumeven =0;
+// int sumodd =0;
+// for (int i=1;i<=10;i++)
+//   if(i%2==0){
+//     sumeven=sumeven+i;
+//   }
+//    print("sum of even numbers = $sumeven");
+
+// for(int j=1;j<=10;j++)
+// if(j%2 !=0){
+//   sumodd=sumodd+j;
+// }
+// print("sum of odd numbers = $sumodd");
+// }
+
+
+//multiplication table
+
+// int i=1;
+// print("Enter a number");
+// int num = int.parse(stdin.readLineSync()!);
+// for(int i=1;i<=10;i++)
+
+// print("$num * $i=${num*i}");
+// }
+
+
+// 3*1=3
+
+}
+
+
+  
+   
+  
+
+
+
+
+
+
+  
+
+
