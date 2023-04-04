8700adf4a574886dc1617c2887f52cfacf562503
Hrithin1010 <hrithinak@gmail.com>
2023-04-04 09:44:32 +0530

    commit 6

--- dart_application_1/bin/iterationstatements.dart
+++ dart_application_1/bin/iterationstatements.dart
@@ -33,8 +33,8 @@ void main(){
 // for(int num=1;num<=10;num++){
 //   sum=sum+num;
 // }
-// print('sum=$sum');
-// }
+// print('sum of natural numbers =$sum');
+
 
 // even numbers sum
 // for(int i=1;i<=20;i++){
@@ -60,19 +60,20 @@ void main(){
 
 
 // int sumeven =0;
-// int sumodd =0;
 // for (int i=1;i<=10;i++)
 //   if(i%2==0){
 //     sumeven=sumeven+i;
 //   }
 //    print("sum of even numbers = $sumeven");
 
+// int sumodd = 0;
 // for(int j=1;j<=10;j++)
 // if(j%2 !=0){
 //   sumodd=sumodd+j;
 // }
 // print("sum of odd numbers = $sumodd");
 // }
+
 
 
 //multiplication table
@@ -86,9 +87,42 @@ void main(){
 // }
 
 
-// 3*1=3
 
+// int i=1;
+// for(int i=1;i<=10;i++);
+// print("$num * $i=${num * i}");
+
+// }
+
+
+
+//sum of even number and odd number in single program
+
+// int evensum=0;
+// int oddsum=0;
+
+// for(int num=1;num<=10;num++){
+//   if(num%2 == 0)
+//   evensum = evensum+num;
+//   else if(num%2 !=0)
+//   oddsum=oddsum+num;
+// }
+// print("sum of even numbers = $evensum");
+// print("sum of odd numbers = $oddsum");
+
+// }
+
+
+
+// multiples of 3 & 5 between 1 to 25 
+int i = 1;
+  for (i = 1; i <= 25; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      print(i);
+    }
+  }
 }
+
 
 
   
