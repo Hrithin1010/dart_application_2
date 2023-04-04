571728c149b0213b4fad7940414a44e6e900e221
Hrithin1010 <hrithinak@gmail.com>
2023-04-04 11:42:18 +0530

    commit 8

---
+++ dart_application_1/bin/functions.dart
@@ -0,0 +1,60 @@
+import 'package:test/test.dart';
+
+void main(){
+// func1();
+// func2("hrithin",22,3);
+// String d = func3();
+// int a=func4();
+// func5(4, 5, "hai");
+// int b =func5(4,5,"hai");
+// print(b);
+// print(func5(100,200,"hai"));
+
+
+
+
+}
+
+//1. functions without return type and parameters
+
+// void func1(){
+//   print("function 1");
+
+// }
+
+
+// //2. funtions without return type and with parameters
+
+// void func2(String name, int age, double cgpa){
+
+//   print("name = $name");
+//   print("age = $age");
+//   print("cgpa = $cgpa");
+
+// }
+
+
+//3. function with return type and without parameters
+
+// String func3(){
+//   String data = "Hello";
+//   print(data);
+//   return data;
+// }
+
+// int func4(){
+//   int a=20;
+//   print(a);
+//   return a;
+// }
+
+
+//4.function with return type and without parameters
+
+// int func5(int a, int b, String c){
+//   int sum = a+b;
+//   print('sum=$sum , $c');
+//   return b;
+// }
+
+
