99410c3345318c0be6ce47bfb0b7f917ff040773
Hrithin1010 <hrithinak@gmail.com>
2023-04-11 11:34:11 +0530

    commit8

---
+++ dart_application_1/bin/palindrome.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+void main(){
+//   print("enter a number");
+//   int num = int.parse(stdin.readLineSync()!);
+//   int rem ;
+//   int rev=0;
+//   int temp=num;
+
+//    while(num>0){
+//     rem=num % 10; // 123  123 % 10 = 12.3= 3(reminder)
+//     rev=(rev *10) + rem; // 0*10 + 3=3
+//     num=num~/10;// 123~/10=12  tilt div.10/12/1/0
+
+//    }
+//    if(temp == rev){
+//     print("$temp is pallindrome");
+
+//    }else{
+//     print("$temp is  not palindrome");
+
+//    }
+// }
+
+
+
+
+//used by string
+
+// print("Enter a value");
+// String? data = stdin.readLineSync();
+// String? rev1 = data?.split("").reversed.join();
+// print("reversed value :$rev1");
+// if(data == rev1){
+// print("$data is palindrome");
+// }else{
+//   print("$data is not palindrome");
+// }
+// }
+
+
+//Factorial
+
+  
+//   int num =5;
+//   int result =1;
+//   for (int i = 1; i <= num; i++) {
+//     result *= i;
+//   }
+
+//   print('Factorial of $num');
+//   print(result);
+// }
+
+
+
+//Check whether the given number is prime or not
+
+
+// print("Enter a number");
+// int num = int.parse(stdin.readLineSync()!);
+// var prime=0;
+
+//  for(int i=2; i<num/2; i++){
+//   if(num%i ==0){
+//     prime=1;
+//     break;
+//   }
+//  }
+
+// if(prime==0){
+//   print("$num is prime");
+
+// }else{
+//   print("$num is not prime");
+// }
+// }
+
+//i=2  2<0 if 6%2==0 true prime = 1 break exit from for loop
+//num = 11
+///i= 2 2<5 if 11%2==0  false skip if part prime =0
+///i=3 3<5 if 11%3 ==0  false skip if part ""
+///i=4 4<5 if 11%4 ==0  false skip if part ""
+///i=5 5<5 if 11%5 ==0 false skip if part ""
+///
+
+
+
+
+//fiboncci series
+
+int n1 =0;
+int n2 =0;
+int n3;
+print(n1);
+print(n2);
+for(int i= 2; i<= 10;i++);
+n3=n1+n2;
+print(n3);
+n1=n2;
+n2=n3;
+}
+
+
+
+
